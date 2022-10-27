8e837c9d9635b5d4e9234c9cc2544728e51809b0
Julian Stobbe <Atomtomate@gmx.de>
2022-10-27 16:42:09 +0200

    temporarily removed dependency on Dispersions.jl

--- src/jDMFT.jl
+++ src/jDMFT.jl
@@ -3,13 +3,14 @@ module jDMFT
 using Random, LinearAlgebra, Logging
 using FastGaussQuadrature
 using FFTW
-using Dispersions
+#using Dispersions
+#TODO: tmporary workaround
+const KGrid = Vector{Float64}
 
 export sample
 export MatsubaraFunction, τFunction
 export subtract_tail
-export τIntegrate, τ_to_ω, τ_to_ω_GR, ω_to_τ
-export sample
+export τIntegrate, τ_to_ω, τ_to_ω, ω_to_τ
 
 # impurity solvers
 export impSolve_IPT
