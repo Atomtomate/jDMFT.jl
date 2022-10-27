557b5e8dff8805570fa0b2a736e28bdd28d97b87
Julian Stobbe <Atomtomate@gmx.de>
2022-10-27 17:17:19 +0200

    yet another documentation fix

--- docs/make.jl
+++ docs/make.jl
@@ -5,3 +5,26 @@ Pkg.activate(String(@__DIR__) * "/..")
 using jDMFT
 
 makedocs(sitename="jDMFT")
+
+makedocs(;
+    modules=[jDMFT],
+    authors="Julian Stobbe <Atomtomate@gmx.de> and contributors",
+    repo="https://github.com/Atomtomate/jDMFT.jl/blob/{commit}{path}#L{line}",
+    sitename="jDMFT",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", nothing) == "true",
+        canonical="https://Atomtomate.github.io/jDMFT.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+        "Dependencies" => "deps.md",
+    ],
+)
+
+deploydocs(;
+    branch="gh-pages",
+    devbranch = "master",
+    devurl = "stable",
+    repo="github.com/Atomtomate/jDMFT.jl.git",
+)
